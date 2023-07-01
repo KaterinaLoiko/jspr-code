fc6b18f62420bd60a55ea9a7bdfd0ac6164f1ea2
ekaterinalojko <eloiko@energocomm.ru>
2023-07-01 19:08:41 +0300

    di-java commit

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,47 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private final List<Post> list;
+    private static final AtomicLong maxId = new AtomicLong(0);
 
-  public Post save(Post post) {
-    return post;
-  }
+    public PostRepository() {
+        list = new CopyOnWriteArrayList<>();
+    }
 
-  public void removeById(long id) {
-  }
+    public List<Post> all() {
+        return list;
+    }
+
+    public Optional<Post> getById(long id) {
+        return list.stream().filter(post -> post.getId() == id).findFirst();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            list.add(new Post(maxId.incrementAndGet(), post.getContent()));
+        } else {
+            Optional<Post> existedPost = getById(post.getId());
+            if (existedPost.isPresent()) {
+                list.set(list.indexOf(existedPost.get()), post);
+            } else {
+                if (post.getId() > maxId.get()) {
+                    maxId.getAndUpdate(n -> post.getId());
+                }
+                list.add(post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        Optional<Post> post = getById(id);
+        post.ifPresent(list::remove);
+    }
 }
